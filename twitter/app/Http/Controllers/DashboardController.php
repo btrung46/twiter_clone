f844d0b16425c2697391d3327643f16c90db1be0
bTrung <nguyenviethoang040603@gmail.com>
2024-10-24 22:39:39 +0700

    created idea models

--- twitter/app/Http/Controllers/DashboardController.php
+++ twitter/app/Http/Controllers/DashboardController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Idea;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('dashboard');
+        $idea = new Idea([
+            'content' =>'hello hoang',
+        ]);
+        $idea->save();
+
+
+        return view('dashboard',[
+            'ideas' => Idea::orderBy("created_at","DESC")->get()
+        ]);
     }
 }
--- twitter/resources/views/dashboard.blade.php
+++ twitter/resources/views/dashboard.blade.php
@@ -52,6 +52,7 @@
             </div>
         </div>
         <hr>
+        @foreach ($ideas as $idea)
         <div class="mt-3">
             <div class="card">
                 <div class="px-3 pt-4 pb-2">
@@ -68,19 +69,16 @@
                 </div>
                 <div class="card-body">
                     <p class="fs-6 fw-light text-muted">
-                        comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
-                        of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of
-                        ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum
-                        dolor sit amet..", comes from a line in section 1.10.32.
+                        {{$idea -> content}}
                     </p>
                     <div class="d-flex justify-content-between">
                         <div>
                             <a href="#" class="fw-light nav-link fs-6"> <span class="fas fa-heart me-1">
-                                </span> 100 </a>
+                                </span> {{$idea -> likes}} </a>
                         </div>
                         <div>
                             <span class="fs-6 fw-light text-muted"> <span class="fas fa-clock"> </span>
-                                3-5-2023 </span>
+                                {{$idea -> created_at}} </span>
                         </div>
                     </div>
                     <div>
@@ -90,34 +88,13 @@
                         <div>
                             <button class="btn btn-primary btn-sm"> Post Comment </button>
                         </div>
-
-                        <hr>
-                        <div class="d-flex align-items-start">
-                            <img style="width:35px" class="me-2 avatar-sm rounded-circle"
-                                src="https://api.dicebear.com/6.x/fun-emoji/svg?seed=Luigi"
-                                alt="Luigi Avatar">
-                            <div class="w-100">
-                                <div class="d-flex justify-content-between">
-                                    <h6 class="">Luigi
-                                    </h6>
-                                    <small class="fs-6 fw-light text-muted"> 3 hour
-                                        ago</small>
-                                </div>
-                                <p class="fs-6 mt-3 fw-light">
-                                    and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and
-                                    Evil)
-                                    by
-                                    Cicero, written in 45 BC. This book is a treatise on the theory of ethics,
-                                    very
-                                    popular during the Renaissan
-                                </p>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
         </div>
+        @endforeach
     </div>
+
     <div class="col-3">
         <div class="card">
             <div class="card-header pb-0 border-0">
